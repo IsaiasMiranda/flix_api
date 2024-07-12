fcd39c98b64630e0d907b5c477b73a69921a41ce
Isaias Miranda <isaias.analise@gmail.com>
2024-07-12 15:15:27 -0300

    troca de base

--- core/settings.py
+++ core/settings.py
@@ -72,10 +72,25 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': 'RGN_STG',
+        'USER': 'powerbi_w',
+        'PASSWORD': 'ismtech@write',
+        'HOST': '10.2.50.23',
+        'PORT': '1433',
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+            'extra_params': 'TrustServerCertificate=yes',
+        },
     }
 }
 
