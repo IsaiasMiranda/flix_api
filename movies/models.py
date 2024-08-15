de929953f3b3a046f3e80a4d7f881551152a1481
Isaias Miranda <isaias.analise@gmail.com>
2024-08-15 11:58:14 -0300

    Ajuste de model movies

--- movies/models.py
+++ movies/models.py
@@ -6,8 +6,8 @@ from genres.models import Genre
 
 class Movie(models.Model):
     title = models.CharField(max_length=500)
-    release_date = models.DateField(null=True, blank=True)
     genre = models.ForeignKey(Genre, on_delete=models.PROTECT, related_name='movies')
+    release_date = models.DateField(null=True, blank=True)
     actors = models.ManyToManyField(Actor, related_name='movies')
     resume = models.TextField(blank=True, null=True)
 
