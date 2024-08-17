7cf0b5373eed1c0930911aa8663cca5ffb3e4114
Isaias Miranda <isaias.analise@gmail.com>
2024-08-17 18:23:33 -0300

    ajuste allowed_hosts

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-31qkgf2&werv@r&@&an9=fe^_1j0oclr!^yqx2#e5vmm@^(&to
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['appflixapi.local', '127.0.0.1', 'localhost']
 
 
 # Application definition
