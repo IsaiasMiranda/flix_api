3cb173ab875a8685355a375b6d01767b89f970a8
Isaias Miranda <isaias.analise@gmail.com>
2024-08-19 23:18:50 -0300

    sql_server.pyodbc

--- core/settings.py
+++ core/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'mssql',
+        'ENGINE': 'sql_server.pyodbc',
         'NAME': 'RGN_STG',
         'USER': 'powerbi_w',
         'PASSWORD': 'ismtech@write',
