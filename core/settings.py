08d52d21f4fed5cfb614cc9d857aa87faf15902e
Isaias Miranda <isaias.analise@gmail.com>
2024-09-02 14:15:41 -0300

    settings debug

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-31qkgf2&werv@r&@&an9=fe^_1j0oclr!^yqx2#e5vmm@^(&to'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['appflixapi.local']
 
